edfa7015939d1258ca9edb3e754e0c8917a3d4ed
devon <devon.geertsen@gmail.com>
2022-11-27 11:15:03 -0700

    Labs Part 2

--- src/Lab.java
+++ src/Lab.java
@@ -1,5 +1,8 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class Lab {
 
@@ -38,24 +41,46 @@ public class Lab {
 				
 		//write and test a method that takes a list of strings and a string and returns a new list with all strings from the original list
 		//containing the second string parameter (i.e. like a search method)
+		List<String > searchResults=search(strings, "a");
+		for (String result:searchResults)
+			System.out.println(result);
 		
 				
 		//write and test a method that takes a list of integers and returns a List<List<Integer>> where the first list in the returned value
 		//contains any number from the input list that is divisible by 2, the second list contains values from the input list that are divisible
 		//by 3, the third divisible by 5, and the fourth all numbers from the input list not divisible by 2, 3, or 5
-	
-				
-		//write and test a method that takes a list of strings and returns a list of integers that contains the length of each string
+		List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9,10,15,20,23,25,30);
+		List<List<Integer>> sortedNumbers = sortDivisibleNumbers(numbers);
 		
+		for(List<Integer> list: sortedNumbers)
+		{
+			for(Integer number:list)
+				System.out.println(number);
+			System.out.println("Next List: ");
+		}
+		//write and test a method that takes a list of strings and returns a list of integers that contains the length of each string
+		List<Integer> lengthList=lengthCalc(strings);
+		for (Integer lenght:lengthList)
+			System.out.println(lenght);
 				
 		//create a set of strings and add 5 values
+		Set<String> set=new HashSet<String>();
+		set.add("Hello World");
+		set.add("Hello Billy");
+		set.add("ello Joey");
+		set.add("Hello Big Jim");
+		set.add("ello Slim Jim");
 				
 		//write and test a method that takes a set of strings and a character and returns a set of strings consisting of all the strings in the
 		//input set that start with the character parameter.
-			
+		Set<String> startsWithH=findThatStartWith(set,'H');
+		for(String word:startsWithH)
+			System.out.println(word);
 				
 		//write and test a method that takes a set of strings and returns a list of the same strings
-	
+		List<String> convertList=convertSetToList(set);
+		for (String string:convertList)
+			System.out.println(string);
 				
 				
 		//write and test a method that takes a set of integers and returns a new set of integers containing only even numbers from the original set
@@ -72,8 +97,70 @@ public class Lab {
 		//with a given character
 
 	}
+	
+	public static Set<String> findThatStartWith(Set<String> set, char c){
+		Set<String> results = new HashSet<String>();
+		for(String string: set)
+			if(string.charAt(0)==c)
+				results.add(string);
+		
+		
+		return results;
+	}
+	
+	public static List<String> convertSetToList(Set<String> set){
+		List<String> results=new ArrayList<String>();
+		for (String string:set)
+			results.add(string);
+		
+		return results;
+	}
+	
+	public static List<Integer> lengthCalc(List<String> strings) {
+		// TODO Auto-generated method stub
+		List<Integer> lengths = new ArrayList<Integer>();
+		for(String string:strings)
+			lengths.add(string.length());
+			
+			
+		return lengths;
+	}
 
-	private static String joinedList(List<String> strings) {
+	public static List<List<Integer>> sortDivisibleNumbers(List<Integer> list){
+		List<List<Integer>> results = new ArrayList<List<Integer>>();
+		results.add(new ArrayList<Integer>());
+		results.add(new ArrayList<Integer>());
+		results.add(new ArrayList<Integer>());
+		results.add(new ArrayList<Integer>());
+		
+		for (Integer number:list) {
+			if(number%2==0)
+				results.get(0).add(number);
+			if(number%3==0)
+				results.get(1).add(number);
+			if(number%5==0)
+				results.get(2).add(number);
+			if(number%2!=0 && number%3!=0 && number%5!=0)
+				results.get(3).add(number);
+			
+		}
+		return results;
+			
+	}
+
+	public static List<String> search(List<String> list, String query){
+		List<String> results = new ArrayList<String>();
+		for(String string:list) {
+			if(string.contains(query)) {
+				results.add(string);
+			}
+		}
+		return results;
+			
+		
+	}
+	
+	public static String joinedList(List<String> strings) {
 		// TODO Auto-generated method stub
 		StringBuilder output = new StringBuilder();
 		for(int i=0; i<strings.size();i++) {
@@ -85,7 +172,7 @@ public class Lab {
 		return output.toString();
 	}
 
-	private static String switched(List<String> strings) {
+	public static String switched(List<String> strings) {
 		String temp=strings.get(0);
 		strings.set(0,strings.get(strings.size()-1));
 		strings.set(strings.size()-1,temp);
